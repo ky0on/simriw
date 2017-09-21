b18ff18a8dcb8d7ae65898430ef916d745b71980
kyon <kyon.0308@gmail.com>
2017-09-21 22:20:04 +0900

    v1.0 of simriw.py

--- simriw.py
+++ simriw.py
@@ -5,11 +5,12 @@
 
 from __future__ import print_function
 import argparse
+import os
 import hjson
+import warnings
 import numpy as np
 import pandas as pd
-import warnings
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 import getwth
 
@@ -58,11 +59,12 @@ if __name__ == '__main__':
     parser.add_argument('--startday', '-s', default='2000-05-15', type=str)
     parser.add_argument('--co2', default=350, type=int)
     parser.add_argument('--transplant', action='store_true')
+    parser.add_argument('--out', default='output', type=str)
     args = parser.parse_args()
 
     #init
     cultivar_params_file = './cultivars.hjson'
-    # plt.style.use('ggplot')
+    plt.style.use('ggplot')
 
     #load cultivar parameters
     cultivar = load_cultivar_params(args.cultivar)
@@ -138,10 +140,10 @@ if __name__ == '__main__':
         simday += 1
         if day >= wth['w'].shape[0]:
             warnings.warn('reached end of weather records')
-            growing = Flse
+            growing = False
 
-        date = wth['w'].loc[day, 'date']
-        res[date] = {
+        res[simday] = {
+            'date': wth['w'].loc[day, 'date'],
             'TMP': AVT[day],
             'RAD': RAD[day],
             'DL': DL[day],
@@ -155,110 +157,109 @@ if __name__ == '__main__':
         #Culculation of Developmental Index DVI
         if DVI < cultivar['DVIA']:
             EFT = AVT[day] - cultivar['TH']
-            DVR = 1. / (cultivar['GV'] * (1.0 + exp(-cultivar@ALF * EFT)))
-        } else if (DVI <= 1.0) {
-            EFT = AVT[day]-cultivar@TH
-            EFL = min(DL[day]-cultivar@LC,0.)
-            DVR = (1.0-exp(cultivar@BDL*EFL))/(cultivar@GV*(1.0+exp(-cultivar@ALF*EFT)))
-        } else {
-            EFT = max(AVT[day]-cultivar@TCR,0.)
-            DVR = (1.0-exp(-cultivar@KCR*EFT))/cultivar@GR
-        }
-        DVI = DVI+DVR
+            DVR = 1. / (cultivar['GV'] * (1.0 + np.exp(-cultivar['ALF'] * EFT)))
+        elif DVI <= 1.0:
+            EFT = AVT[day] - cultivar['TH']
+            EFL = min(DL[day] - cultivar['LC'], 0.)
+            DVR = (1.0 - np.exp(cultivar['BDL'] * EFL)) / (cultivar['GV'] * (1.0 + np.exp(-cultivar['ALF'] * EFT)))
+        else:
+            EFT = max(AVT[day] - cultivar['TCR'], 0.)
+            DVR = (1.0 - np.exp(-cultivar['KCR'] * EFT)) / cultivar['GR']
+        DVI += DVR
 
         #Culculation of LAI
-        if (DVI < 0.95) {
-            EFFTL = max(AVT[day]-cultivar@TCF,0.)
-            GRLAI = LAI*cultivar@A*(1.0-exp(-cultivar@KF*EFFTL))*(1.0-(LAI/cultivar@FAS)**cultivar@ETA)
+        if DVI < 0.95:
+            EFFTL = max(AVT[day] - cultivar['TCF'], 0.)
+            GRLAI = LAI * cultivar['A'] * (1.0 - np.exp(-cultivar['KF'] * EFFTL)) * (1.0 - (LAI/cultivar['FAS'])**cultivar['ETA'])
             GRL95 = GRLAI
             DVI95 = DVI
-        } else if (GRLAI > 0.0  |  DVI <= 1.0) {
-            GRLAI = GRL95*(1.0-(DVI-DVI95)/(1-DVI95))
+        elif GRLAI > 0.0 or DVI <= 1.0:
+            GRLAI = GRL95 * (1.0 - (DVI - DVI95)/(1 - DVI95))
             LAIMX = LAI
             DVI1 = DVI
-        } else if (DVI < 1.1) {
-            GRLAI = -(LAIMX*(1.0-cultivar@BETA)*(DVI-DVI1)/(1.1-DVI1))*DVR
-        } else {
-            GRLAI = -LAIMX*(1.0-cultivar@BETA)*DVR
-        }
-        LAI = LAI+GRLAI
+        elif DVI < 1.1:
+            GRLAI = -(LAIMX * (1.0 - cultivar['BETA']) * (DVI - DVI1)/(1.1 - DVI1))*DVR
+        else:
+            GRLAI = -LAIMX * (1.0 - cultivar['BETA']) * DVR
+        LAI += GRLAI
 
         #Culuculation of Crop Dry Weight
-        TAU = exp(-(1.0-SCAT)*cultivar@EXTC*LAI)
-        REF = RCAN-(RCAN-RSOL)*exp(-KREF*LAI)
-        ABSOP = 1.0-REF-(1.0-RSOL)*TAU
-        ABSRAD = RAD[day]*ABSOP
-        COVCO2 = cultivar@COVES*(1.54*(CO2-330.0)/(1787.0+(CO2-  330.0))+1.0)
-        if (DVI < 1.0) {
+        TAU = np.exp(-(1.0 - SCAT) * cultivar['EXTC'] * LAI)
+        REF = RCAN - (RCAN - RSOL) * np.exp(-KREF * LAI)
+        ABSOP = 1.0 - REF - (1.0 - RSOL) * TAU
+        ABSRAD = RAD[day] * ABSOP
+        COVCO2 = cultivar['COVES'] * (1.54 * (args.co2 - 330.0)/(1787.0+(args.co2 - 330.0))+1.0)
+        if DVI < 1.0:
             CONEF = COVCO2
-        } else {
-            CONEF = COVCO2*(1.0+CL)/(1.0+CL*exp((DVI-1.0)/TAUC))
-        }
-        DW = DW+CONEF*ABSRAD
+        else:
+            CONEF = COVCO2 * (1.0+CL)/(1.0+CL * np.exp((DVI - 1.0)/TAUC))
+        DW = DW + CONEF * ABSRAD
 
         #Culuculation of Spikelet Sterility Percentage due to Cool Temerature
-        if (DVI > 0.75  &  DVI < 1.2) {
-            CDEG = max(cultivar@THOT-AVT[day],0.)
-            CDED = CDED+CDEG
-            SSTR = cultivar@STO+cultivar@BST*CDED**cultivar@PST
-            STLT = min(100.0,SSTR)
-            RIPEP = 1.0-STLT/100.0
-            ATHLT = cultivar@HIMX*RIPEP
-        }
+        if DVI > 0.75 and DVI < 1.2:
+            CDEG = max(cultivar['THOT'] - AVT[day], 0.)
+            CDED += CDEG
+            SSTR = cultivar['STO']+cultivar['BST'] * CDED**cultivar['PST']
+            STLT = min(100.0, SSTR)
+            RIPEP = 1.0 - STLT/100.0
+            ATHLT = cultivar['HIMX'] * RIPEP
 
         #Culculation of Spikelet Sterility Percentage due to Heat Stress
-        if (DVI > 0.96  &  DVI < 1.20) {
-            HTSUM = HTSUM+TMX[day]
-            HTDAY = HTDAY+1
-        }
-        if (DVI >= 1.20  &  IFLUG1 == 0) {
-            AVTMX = HTSUM/HTDAY
-            STHT = 100.0/(1.0+exp(-0.853*(AVTMX-36.6)))
-            ATHHT = (1.0-STHT/100.0)*cultivar@HIMX
+        if DVI > 0.96 and DVI < 1.20:
+            HTSUM += TMX[day]
+            HTDAY += 1
+
+        if DVI >= 1.20 and IFLUG1 == 0:
+            AVTMX = HTSUM / HTDAY
+            STHT = 100.0/(1.0 + np.exp(-0.853 * (AVTMX - 36.6)))
+            ATHHT = (1.0 - STHT/100.0) * cultivar['HIMX']
             IFLUG1 = 1
-        }
 
         #Culculation of Grain Yield
-        ATHI = min(ATHLT,ATHHT)
-        STERP = max(STHT,STLT)
-        EFDVI = max(DVI-1.22, 0.0)
-        HI = ATHI*(1.0-exp(-5.57*EFDVI))
-        DWGRAIN = DW*HI
+        ATHI = min(ATHLT, ATHHT)
+        STERP = max(STHT, STLT)
+        EFDVI = max(DVI - 1.22, 0.0)
+        HI = ATHI * (1.0 - np.exp(-5.57 * EFDVI))
+        DWGRAIN = DW * HI
         DWPAN = DWGRAIN/CVBP/CVPP
 
         #Time Control and Terminal Condition of Simulation
-        if (DVI > 1.0  &  AVT[day] < cultivar@CTR) {
-            TCHECK = TCHECK+1
-        }
-        if (DVI > 2.0) {
-            growing = FALSE
-        }
+        if DVI > 1.0 and AVT[day] < cultivar['CTR']:
+            TCHECK += 1
+        if DVI > 2.0:
+            growing = False
 
-    # simday <- simday + 1
-    # res[simday,'date'] <- wth@w$date[day]
-    # res[simday,'TMP'] <- AVT[day]
-    # res[simday,'RAD'] <- RAD[day]
-    # res[simday,'DL'] <- DL[day]
-    # res[simday,'DVI'] <- DVI
-    # res[simday,'LAI'] <- LAI
-    # res[simday,'DW'] <- DW
-    # res[simday,'GY'] <- DWGRAIN
-    # res[simday,'PY'] <- DWPAN
-    #
-    # #Terminal Section of  Simulation
-    # PYBROD <- DWGRAIN/100.0
-    # PYBRON <- PYBROD/0.86
-    # PYPADY <- PYBRON/CVBP
-    # PANDW <- PYBROD/CVBP/CVPP
-    # DWT <- DW/100.0
-    #
-    # r <- new('SIMRIW')
-    # r@cultivar <- cultivar
-    # r@PYBROD <- PYBROD
-    # r@PYBRON <- PYBRON
-    # r@PYPADY <- PYPADY
-    # r@PANDW <- PANDW
-    # r@DWT <- DWT
-    # r@d <- res[1:simday,]
-    #
-    # return(r)
+    #finalize
+    simday += 1
+    res[simday] = {
+        'date': wth['w'].loc[day, 'date'],
+        'TMP': AVT[day],
+        'RAD': RAD[day],
+        'DL': DL[day],
+        'DVI': DVI,
+        'LAI': LAI,
+        'DW': DW,
+        'GY': DWGRAIN,
+        'PY': DWPAN,
+    }
+
+    #Terminal Section of  Simulation
+    PYBROD = DWGRAIN / 100.0
+    PYBRON = PYBROD / 0.86
+    PYPADY = PYBRON / CVBP
+    PANDW = PYBROD / CVBP / CVPP
+    DWT = DW / 100.0
+
+    #simulation result
+    simulated = {}
+    simulated['cultivar'] = cultivar
+    simulated['PYBROD'] = PYBROD
+    simulated['PYBRON'] = PYBRON
+    simulated['PYPADY'] = PYPADY
+    simulated['PANDW'] = PANDW
+    simulated['DWT'] = DWT
+    simulated['d'] = pd.DataFrame(res).T   # [1:simday, ]
+
+    #plot
+    simulated['d'][['DW', 'GY', 'PY']].plot()
+    plt.savefig(os.path.join(args.out, 'simulated.pdf'))
