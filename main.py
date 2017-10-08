ca5b0628308e77196790a23f1bd60b3de663d6f6
kyon <kyon.0308@gmail.com>
2017-10-08 15:31:35 +0900

    run simulation with my dataset

--- main.py
+++ main.py
@@ -5,48 +5,58 @@
 
 from __future__ import print_function
 import os
-import re
-import hjson
+import glob
 import argparse
-# import numpy as np
-import pandas as pd
-# import matplotlib.pyplot as plt
+import simriw
+# import pandas as pd
+import matplotlib.pyplot as plt
 
 __autor__ = 'Kyosuke Yamamoto (kyon)'
 __date__ = '08 Oct 2017'
+
+
+def load_config(csvpath):
+    ''' Load configs in csvfile '''
+    config = {}
+    f = open(csvpath)
+    lines = f.readlines()
+    f.close()
+    for line in lines:
+        if not line[0] == '#':
+            break
+        else:
+            if line[:10] == '#config - ':
+                _config = line.replace('#config - ', '').replace('\n', '')
+                key, value = _config.split(':')
+                try:
+                    config[key] = float(value)
+                except ValueError:
+                    config[key] = str(value)
+
+    return config
+
 
 if __name__ == '__main__':
 
     #argparse
     parser = argparse.ArgumentParser()
-    # parser.add_argument('hoge')
-    parser.add_argument('path', nargs='?', type=str, default='./history_with_yield.hjson')
-    # parser.add_argument('path', nargs='+', type=str, help='one or more arguments')
-    # parser.add_argument('path', nargs='*', type=str, help='one or more args. uses default if empty.')
-    # parser.add_argument('--model', '-m', default='conv3layer', type=str, help='model type')
-    # parser.add_argument('--foo', '-f', action='store_true')
+    parser.add_argument('path', nargs='?', type=str, default='./dataset')
+    parser.add_argument('--out', '-o', default='output', type=str)
     args = parser.parse_args()
 
-    #init
-    # plt.style.use('ggplot')
-    tmpout = './tmp'
-
-    #load dataset
-    with open(args.path, 'r') as f:
-        history = hjson.load(f)
-
     #main
-    for key in history.keys():
+    for csvpath in glob.glob(os.path.join(args.path, '*.csv')):
 
-        #json -> csv
-        csv = '#config - lat:{}\n'.format(history[key]['lat'])
-        csv += '#config - lon:{}\n'.format(history[key]['lon'])
-        # csv += '#config - alt:{}\n'.format(history[key]['alt'])
-        mesh = pd.read_json(history[key]['mesh'])
-        mesh.index.name = 'DATE'
-        csv += re.sub(' +', ',', mesh.reset_index().to_string(index=False))
-        outcsv = os.path.join(tmpout, key + '.csv')
-        with open(outcsv, 'w') as f:
-            f.write(csv)
+        #load params
+        print(csvpath)
+        config = load_config(csvpath)
 
         #run simulation
+        simulated = simriw.main(
+            'Nipponbare', csvpath, True, config['planting_date'], 350)
+
+        #fin
+        simulated['d'][['DW', 'GY', 'PY']].plot()
+        plt.savefig(os.path.join(args.out, 'simulated_{}.pdf').format(os.path.basename(csvpath)))
+        simulated['d'].to_csv(os.path.join(args.out, 'simulated_{}.csv').format(os.path.basename(csvpath)))
+        # print('\nsimulated["d"].tail():\n', simulated['d'].tail())
