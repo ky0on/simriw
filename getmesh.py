bd0e640d08da18f8de24167ccbc32af2c6a2dfa2
kyon <kyon.0308@gmail.com>
2017-10-08 10:10:08 +0900

    save mesh as hjson

--- getmesh.py
+++ getmesh.py
@@ -6,13 +6,12 @@
 from __future__ import print_function
 import hjson
 import argparse
+import pandas as pd
+# import os
+# import numpy as np
+# import matplotlib.pyplot as plt
 
 import AMD_Tools3 as AMD
-# import os
-# import glob
-# import numpy as np
-# import pandas as pd
-# import matplotlib.pyplot as plt
 
 __autor__ = 'Kyosuke Yamamoto (kyon)'
 __date__ = '08 Oct 2017'
@@ -43,8 +42,8 @@ if __name__ == '__main__':
         if has_key(['planting_date', 'harvesting_date', 'yield', 'gps'], history_all[key]):
             # print(key)
             history[key] = {}
-            history[key]['planting_date'] = history_all[key]['planting_date']
-            history[key]['harvesting_date'] = history_all[key]['harvesting_date']
+            history[key]['planting_date'] = history_all[key]['planting_date'][:10]
+            history[key]['harvesting_date'] = history_all[key]['harvesting_date'][:10]
             history[key]['yield'] = history_all[key]['yield']
             history[key]['lat'] = float(history_all[key]['gps'].split(',')[0])
             history[key]['lon'] = float(history_all[key]['gps'].split(',')[1])
@@ -56,10 +55,10 @@ if __name__ == '__main__':
         lalodomain = [history[key]['lat'], history[key]['lat'], history[key]['lon'], history[key]['lon']]
         Msh, tim, lat, lon = AMD.GetMetData(element, timedomain, lalodomain)
         Msh = Msh[:, 0, 0]     # dimension (from 3 to 1)
-        print(Msh)
-        # mesh = pd.DataFrame({'meshtemp': Msh})
-        # mesh.set_index('date', inplace=True)
+        mesh = pd.DataFrame({'date': tim, 'airtemp': Msh})
+        mesh.set_index('date', inplace=True)
+        history[key]['mesh'] = mesh.to_json()
 
-        #concat and save
-        # df = pd.concat([ek.airtemp, mesh], axis=1)
-        # df.to_csv(os.path.join(args.out, key + '.csv'))
+    #save
+    with open('history_with_yield.hjson', 'w') as f:
+        hjson.dump(history, f)
