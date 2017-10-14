ed3bf51858149bc69c3c5f335a72bae919a2914e
kyon <kyon.0308@gmail.com>
2017-10-14 10:53:19 +0900

    get yamanashi data

--- gridsimulation/getmesh.py
+++ gridsimulation/getmesh.py
@@ -4,7 +4,6 @@
 """   """
 
 from __future__ import print_function
-import os
 import hjson
 import argparse
 import numpy as np
@@ -30,46 +29,66 @@ if __name__ == '__main__':
     #explore locations
     for pref in dataset.keys():
 
+        #filter by prefecture
+        if pref != 'yamanashi':
+            continue
+
         #init
         lalodomain = [dataset[pref]['lat0'], dataset[pref]['lat1'],
                       dataset[pref]['lon0'], dataset[pref]['lon1']]
-        meshes = defaultdict(lambda: [])
+        meshes = {}   # key=name
 
         #explore year
-        for year in range(1980, 2016):
+        for year in range(1980, 2017):
 
             #access to server
+            elements = ('TMP_mea', 'TMP_max', 'TMP_min', 'RH', 'GSR', 'APCP')
             timedomain = ['{}-01-01'.format(year), '{}-12-31'.format(year)]
-            for element in ('TMP_mea', 'TMP_max', 'TMP_min', 'RH', 'GSR', 'APCP'):
+            for element in elements:
                 try:
                     Msh, tim, lat, lon = AMD.GetMetData(element, timedomain, lalodomain)
-                    Msh = Msh[:, 0, 0]     # dimension (from 3 to 1)
+
                 except OSError:
-                    print('Warning: {} in {} is not available'.format(element, year))
-                    Msh = np.nan
-                mesh = pd.DataFrame({'DATE': tim, element: Msh})
-                mesh.set_index('DATE', inplace=True)
-                meshes[element].append(mesh)
+                    print('Warning: {} in {} is not available. Filled with nan'.format(element, year))
+                    Msh = np.empty([len(tim), len(lat), len(lon)])   # assuming other element in the same year has been downloaded just before
+                    Msh[:, :] = np.nan
 
-        #concat
-        meshes = [pd.concat(meshes[element]) for element in meshes.keys()]
-        mesh = pd.concat(meshes, axis=1)
+                #split by meshcode
+                for y in range(len(lat)):
+                    for x in range(len(lon)):
+                        mesh = pd.DataFrame({'DATE': tim, element: Msh[:, y, x]})
+                        mesh.set_index('DATE', inplace=True)
+                        name = '{0}_{1:0>3}_{2:0>3}'.format(pref, y, x)
+                        if name not in meshes.keys():
+                            meshes[name] = {
+                                'lat': lat[y],
+                                'lon': lon[x],
+                                'mesh': defaultdict(lambda: []),
+                            }
+                        meshes[name]['mesh'][element].append(mesh)
 
-        #rename columns
-        mesh['DOY'] = mesh.index.dayofyear
-        mesh['YEAR'] = mesh.index.year
-        mesh.rename(columns={
-            'TMP_mea': 'T2M',
-            'TMP_max': 'T2MX',
-            'TMP_min': 'T2MN',
-            'GSR': 'swv_dwn',
-            'APCP': 'RAIN',
-            'RH': 'RH2M'}, inplace=True)
+        #save as csv
+        for name, dic in meshes.items():
 
-        #save
-        outcsv = dataset[pref]['csv']
-        with open(outcsv, 'w') as f:
-            f.write('#config - lat:{}\n'.format(dataset[pref]['lat']))
-            f.write('#config - lon:{}\n'.format(dataset[pref]['lon']))
-            mesh.to_csv(f)
-        print('saved as', outcsv)
+            #concat
+            _mesh = [pd.concat(dic['mesh'][element]) for element in elements]
+            mesh = pd.concat(_mesh, axis=1)
+
+            #rename columns
+            mesh['DOY'] = mesh.index.dayofyear
+            mesh['YEAR'] = mesh.index.year
+            mesh.rename(columns={
+                'TMP_mea': 'T2M',
+                'TMP_max': 'T2MX',
+                'TMP_min': 'T2MN',
+                'GSR': 'swv_dwn',
+                'APCP': 'RAIN',
+                'RH': 'RH2M'}, inplace=True)
+
+            #save
+            outcsv = 'dataset/{}.csv'.format(name)
+            with open(outcsv, 'w') as f:
+                f.write('#config - lat:{}\n'.format(dic['lat']))
+                f.write('#config - lon:{}\n'.format(dic['lon']))
+                mesh.to_csv(f)
+            print('saved as', outcsv)
