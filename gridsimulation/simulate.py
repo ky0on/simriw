30887cb656ea97d46d38e18ab4534c721c9364f5
kyon <kyon.0308@gmail.com>
2017-10-14 23:25:57 +0900

    use joblib

--- gridsimulation/simulate.py
+++ gridsimulation/simulate.py
@@ -9,6 +9,7 @@ import os
 import sys
 import glob
 import hjson
+import joblib
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
@@ -22,6 +23,45 @@ import simriw
 __autor__ = 'Kyosuke Yamamoto (kyon)'
 __date__ = '09 Oct 2017'
 
+
+def simulate(csvpath):
+
+    simulated_all = []
+
+    #check weather data
+    csv = pd.read_csv(csvpath, comment='#', index_col='DATE', parse_dates=['DATE'])
+    name = csvpath.split('.')[0]
+    if np.any(pd.isnull(csv.T2M)):
+        print(f'np.nan in T2M. Skipped {csvpath}')
+        return None
+
+    #plot raw data
+    if args.plotraw:
+        csv.plot(subplots=True)
+        plt.savefig(os.path.join(outdir, 'raw_{}.jpg'.format(pref)))
+
+    #explore year
+    # for year in range(1980, 2017):
+    for year in range(1980, 1982):
+
+        #init
+        start_date = str(year) + '-' + dataset[pref]['planting']
+
+        #run simulation
+        simulated = simriw.main(
+            'Koshihikari', csvpath, True, start_date, 350,
+            '../cultivars.hjson', silent=True
+        )
+        simulated['d']['year'] = year
+        simulated['d']['name'] = name
+        simulated_all.append(simulated['d'])
+
+        #fin
+        # simulated['d'].to_csv(os.path.join(outdir, 'sim_{}_{}.csv').format(pref, year))
+
+    return simulated_all
+
+
 if __name__ == '__main__':
 
     #argparse
@@ -32,7 +72,7 @@ if __name__ == '__main__':
     #init
     plt.style.use('ggplot')
     outdir = 'output'
-    simulated_all = []
+    result = []
 
     #load dataset settings
     with open('dataset.hjson', 'r') as f:
@@ -49,42 +89,12 @@ if __name__ == '__main__':
         csvpaths = glob.glob(os.path.join('dataset', '{}*.csv'.format(pref)))
         csvpaths.sort()
 
-        for csvpath in tqdm(csvpaths):
-
-            #check weather data
-            csv = pd.read_csv(csvpath, comment='#', index_col='DATE', parse_dates=['DATE'])
-            name = csvpath.split('.')[0]
-            if np.any(pd.isnull(csv.T2M)):
-                print(f'np.nan in T2M. Skipped {csvpath}')
-                continue
-
-            #plot raw data
-            if args.plotraw:
-                csv.plot(subplots=True)
-                plt.savefig(os.path.join(outdir, 'raw_{}.jpg'.format(pref)))
-
-            #explore year
-            # for year in range(1980, 2017):
-            for year in range(1980, 1982):
-
-                #init
-                start_date = str(year) + '-' + dataset[pref]['planting']
-
-                #run simulation
-                simulated = simriw.main(
-                    'Koshihikari', csvpath, True, start_date, 350,
-                    '../cultivars.hjson', silent=True
-                )
-                simulated['d']['year'] = year
-                simulated['d']['name'] = name
-                simulated_all.append(simulated['d'])
-
-                #fin
-                # simulated['d'].to_csv(os.path.join(outdir, 'sim_{}_{}.csv').format(pref, year))
+        r = joblib.Parallel(n_jobs=4, verbose=1)(joblib.delayed(simulate)(csvpath) for csvpath in csvpaths[2300:2400])
+        result.append(pd.concat([pd.concat(_r) for _r in r if _r is not None]))
 
     #convert result to dataframe
-    simulated_all = pd.concat(simulated_all)
-    simulated_all.to_csv(os.path.join(outdir, 'sim.csv'), index=False)
+    result = pd.concat(result)
+    result.to_csv(os.path.join(outdir, 'sim.csv'), index=False)
 
     #plot GY in subplots
     # for loc in simulated_all['loc'].unique():
