534c9d801dd0f58319946d58bc2236259a75bb54
kyon <kyon.0308@gmail.com>
2017-10-22 20:22:39 +0900

    use joblib for dataset loading

--- gridsimulation/ml.py
+++ gridsimulation/ml.py
@@ -5,15 +5,24 @@
 
 from __future__ import print_function
 import argparse
-# import os
-# import glob
+import os
+import glob
+import joblib
 import numpy as np
 import pandas as pd
 from sklearn import tree
+from slacker import Slacker
 import matplotlib.pyplot as plt
 
 __autor__ = 'Kyosuke Yamamoto (kyon)'
 __date__ = '15 Oct 2017'
+
+
+def load_dataset(csvpath):
+    # print('Loading', csvpath)
+    df = pd.read_csv(csvpath)
+    return df
+
 
 if __name__ == '__main__':
 
@@ -23,8 +32,12 @@ if __name__ == '__main__':
 
     #init
     plt.style.use('ggplot')
-    df = pd.read_csv('output/sim.csv')
-    shortest = df.groupby(['name', 'year']).apply(len).min()
+    slack = Slacker(os.environ['SLACK_API_KYONAWS'])
+    csvpaths = glob.glob(os.path.join('simdata', '*.csv'))
+    dfs = joblib.Parallel(n_jobs=-1, verbose=1)(
+        joblib.delayed(load_dataset)(csvpath) for csvpath in csvpaths)
+    df = pd.concat(dfs)
+    shortest = df.groupby(['meshcode', 'year']).apply(len).min()
     x_types = ['DL', 'TMP', 'RAD']
     x, y = [], []
 
@@ -36,9 +49,10 @@ if __name__ == '__main__':
 
     #extract dataset
     xs, ys = [], []
-    for name in df['name'].unique():
-        for year in df.loc[df['name'] == name, :].year.unique():
-            adf = df.loc[(df['name'] == name) & (df['year'] == year), :] 
+    for meshcode in df['meshcode'].unique():
+        for year in df.loc[df['meshcode'] == meshcode, :].year.unique():
+            # print(year, meshcode)
+            adf = df.loc[(df['meshcode'] == meshcode) & (df['year'] == year), :]
             adf = adf.iloc[:shortest, :]
             x = adf[x_types]
             x = x.values.T.flatten()
@@ -49,12 +63,15 @@ if __name__ == '__main__':
     #run machine learning
     xs = np.array(xs)
     ys = np.array(ys)
+    print('xs.shape:', xs.shape)
+    print('ys.shape:', ys.shape)
     clf = tree.DecisionTreeRegressor()
     clf = clf.fit(xs, ys)
     p = clf.predict(xs)
     result = pd.DataFrame({'predicted': p, 'observed': ys})
     result.plot.scatter(x='observed', y='predicted')
-    plt.savefig('output/result.pdf')
+    plt.savefig('output/simresult.pdf')
+    slack.files.upload('output/simresult.pdf', initial_comment='simresult.pdf', channels='#general')
 
     #plot tree
     import pydotplus
@@ -65,3 +82,4 @@ if __name__ == '__main__':
                          filled=True, rounded=True)
     graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
     graph.write_pdf('output/tree.pdf')
+    slack.files.upload('output/tree.pdf', initial_comment='tree.pdf', channels='#general')
