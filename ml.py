a01f53591bbe8d88615fd39ba91a27d81d02ad24
kyon <kyon.0308@gmail.com>
2017-11-09 23:18:35 +0900

    run k-fold cv

--- ml.py
+++ ml.py
@@ -62,29 +62,32 @@ if __name__ == '__main__':
             y = adf.GY.iloc[-1]
             xs.append(x)
             ys.append(y)
-
-    #run machine learning
     xs = np.array(xs)
     ys = np.array(ys)
     print('xs.shape:', xs.shape)
     print('ys.shape:', ys.shape)
-    print('In training...')
-    clf = tree.DecisionTreeRegressor()
-    clf = clf.fit(xs, ys)
-    p = clf.predict(xs)
-    print('done!')
-    result = pd.DataFrame({'predicted': p, 'observed': ys})
-    result.plot.scatter(x='observed', y='predicted')
-    plt.savefig('output/simresult.pdf')
-    slack.files.upload('output/simresult.pdf', initial_comment='simresult.pdf', channels='#general')
 
-    #plot tree
-    import pydotplus
-    from sklearn.externals.six import StringIO
-    dot_data = StringIO()
-    tree.export_graphviz(clf, out_file=dot_data,
-                         feature_names=x_names,
-                         filled=True, rounded=True)
-    graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
-    graph.write_pdf('output/tree.pdf')
-    slack.files.upload('output/tree.pdf', initial_comment='tree.pdf', channels='#general')
+    #run machine learning
+    from sklearn.model_selection import ShuffleSplit
+    ss = ShuffleSplit(n_splits=5, test_size=0.50, random_state=308)
+    for fold, (train, test) in enumerate(ss.split(ys)):
+        clf = tree.DecisionTreeRegressor(max_depth=10)
+        clf = clf.fit(xs[train], ys[train])
+        p = clf.predict(xs)
+        is_test = np.zeros(ys.shape, dtype=bool)
+        is_test[test] = True
+        result = pd.DataFrame({'predicted': p, 'observed': ys, 'is_test': is_test})
+        result.plot.scatter(x='observed', y='predicted', c='is_test')
+        plt.savefig(f'output/simresult{fold}.pdf')
+        # slack.files.upload('output/simresult.pdf', initial_comment='simresult.pdf', channels='#general')
+
+        #plot tree
+        import pydotplus
+        from sklearn.externals.six import StringIO
+        dot_data = StringIO()
+        tree.export_graphviz(clf, out_file=dot_data,
+                             feature_names=x_names,
+                             filled=True, rounded=True)
+        graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+        graph.write_pdf(f'output/tree{fold}.pdf')
+        # slack.files.upload('output/tree.pdf', initial_comment='tree.pdf', channels='#general')
