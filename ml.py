a6459cbe2e6971bc471de79785551f27f7c0c0cd
kyon <kyon.0308@gmail.com>
2017-11-11 22:39:45 +0900

    predict DVR

--- ml.py
+++ ml.py
@@ -34,60 +34,50 @@ if __name__ == '__main__':
     #init
     plt.style.use('ggplot')
     slack = Slacker(os.environ['SLACK_API_KYONAWS'])
-    csvpaths = glob.glob(os.path.join('simdata', '*.csv'))
+
+    #load simdata
+    csvpaths = glob.glob(os.path.join('simdata', '*.csv'))[:3]
     if args.debug:
         csvpaths = csvpaths[:2]
-    dfs = joblib.Parallel(n_jobs=-1, verbose=1)(
+    simdata_all = joblib.Parallel(n_jobs=-1, verbose=1)(
         joblib.delayed(load_dataset)(csvpath) for csvpath in csvpaths)
-    df = pd.concat(dfs)
-    shortest = df.groupby(['meshcode', 'year']).apply(len).min()
-    x_types = ['DL', 'TMP', 'RAD']
-    x, y = [], []
-
-    #x_names
-    x_names = []
-    for t in x_types:
-        for i in range(shortest):
-            x_names.append(f'{t}_day{i:0>3}')
-
-    #extract dataset
-    xs, ys = [], []
-    for meshcode in df['meshcode'].unique():
-        for year in df.loc[df['meshcode'] == meshcode, :].year.unique():
-            # print(year, meshcode)
-            adf = df.loc[(df['meshcode'] == meshcode) & (df['year'] == year), :]
-            adf = adf.iloc[:shortest, :]
-            x = adf[x_types]
-            x = x.values.T.flatten()
-            y = adf.GY.iloc[-1]
-            xs.append(x)
-            ys.append(y)
-    xs = np.array(xs)
-    ys = np.array(ys)
-    print('xs.shape:', xs.shape)
-    print('ys.shape:', ys.shape)
+    simdata = pd.concat(simdata_all)
+    simdata.dropna(how='any', inplace=True)   # drop nan records
 
     #run machine learning
     from sklearn.model_selection import ShuffleSplit
-    ss = ShuffleSplit(n_splits=5, test_size=0.50, random_state=308)
-    for fold, (train, test) in enumerate(ss.split(ys)):
+    ss = ShuffleSplit(n_splits=5, test_size=0.30, random_state=308)
+    for fold, (train, test) in enumerate(ss.split(simdata)):
+
+        #init
+        xd = ['DL', 'TMP', 'RAD', 'DVI']
+        yd = ['DVR']
+
+        #train
         clf = tree.DecisionTreeRegressor(max_depth=10)
-        clf = clf.fit(xs[train], ys[train])
-        p = clf.predict(xs)
-        is_test = np.zeros(ys.shape, dtype=bool)
-        is_test[test] = True
-        result = pd.DataFrame({'predicted': p, 'observed': ys, 'is_test': is_test})
+        clf = clf.fit(simdata.iloc[train][xd],
+                      simdata.iloc[train][yd])
+
+        #test
+        pr = clf.predict(simdata[xd])
+        is_test = np.zeros(simdata.shape[0], dtype=int)
+        is_test[test] = 1
+        result = pd.DataFrame({'predicted': pr, 'observed': simdata[yd].values[:, 0], 'is_test': is_test})
+
+        #plot prediction
         result.plot.scatter(x='observed', y='predicted', c='is_test')
-        plt.savefig(f'output/simresult{fold}.pdf')
-        # slack.files.upload('output/simresult.pdf', initial_comment='simresult.pdf', channels='#general')
+        plt.savefig(f'output/simresult{fold}.png')
+        # slack.files.upload('output/simresult.png', initial_comment='simresult.png', channels='#general')
 
         #plot tree
         import pydotplus
         from sklearn.externals.six import StringIO
         dot_data = StringIO()
-        tree.export_graphviz(clf, out_file=dot_data,
-                             feature_names=x_names,
-                             filled=True, rounded=True)
+        tree.export_graphviz(clf,
+                             out_file=dot_data,
+                             feature_names=xd,
+                             filled=True,
+                             rounded=True)
         graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
-        graph.write_pdf(f'output/tree{fold}.pdf')
-        # slack.files.upload('output/tree.pdf', initial_comment='tree.pdf', channels='#general')
+        graph.write_png(f'output/tree{fold}.png')
+        # slack.files.upload('output/tree.png', initial_comment='tree.png', channels='#general')
