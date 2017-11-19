f4ef50bb561f3fc74c89073ce5f88a6699f49b22
kyon <kyon.0308@gmail.com>
2017-11-19 14:56:40 +0900

    sample df

--- ml.r
+++ ml.r
@@ -2,7 +2,7 @@ set.seed(308)
 
 #load
 library(data.table)
-cat('loading...')
+cat('loading...\n')
 dir <- 'simdata'
 files <- list.files(path=dir, pattern='*.csv')
 temp <- lapply(paste0(dir, '/', files), fread, sep=',')
@@ -11,9 +11,25 @@ df <- rbindlist(temp)
 #train
 library(Cubist)
 library(mlbench)
+library(dplyr)
 cat('training...\n')
-model <- cubist(x=df[, c('DL', 'TMP', 'RAD', 'DVI'), with=F], y=df[['DVR']])
-# summary(model)
+sdf <- sample_n(df, 500000)
+x <- sdf[, c('DL', 'TMP', 'RAD', 'DVI'), with=F]
+y <- sdf[['DVR']]
+model <- cubist(x, y)
+summary(model)
+
+#parameter tuning with caret
+# library(doParallel)
+# require(caret)
+# cl <- makeCluster(8); registerDoParallel(cl)
+# df <- sample_n(df, 100)
+# x <- df[, c('DL', 'TMP', 'RAD', 'DVI'), with=F]
+# y <- df[['DVR']]
+# fit <- train(x, y, 'cubist', committees=1)
+# fit
+# fit$times$everything
+# stopCluster(cl); registerDoSEQ()
 
 #plot
 # library(gridExtra)
