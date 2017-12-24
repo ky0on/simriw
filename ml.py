0926673f7959614dfb0e1da400674bb59ae2456f
kyon <kyon.0308@gmail.com>
2017-12-24 19:11:50 +0900

    add cnn code

--- ml.py
+++ ml.py
@@ -44,6 +44,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--debug', action='store_true')
     args = parser.parse_args()
+    # args.debug = True
 
     #init
     plt.style.use('ggplot')
@@ -82,4 +83,50 @@ if __name__ == '__main__':
     plt.xlabel('GY')
     plt.savefig('output/hist_y.pdf')
 
-    #TODO: learn cnn regression in keras
+    #cnn
+    import keras
+    from keras.models import Sequential
+    from keras.layers import Dense, Dropout, Flatten
+    from keras.layers import Conv2D
+    # from keras.layers import Conv2D, MaxPooling2D
+
+    batch_size = 32
+    epochs = 12
+
+    x_train = xs.reshape(xs.shape[0], xs.shape[1], xs.shape[2], 1)
+    y_train = ys.reshape(ys.shape[0], 1)
+    #TODO(kyon):normalization
+
+    # x_train = x_train.astype('float32')
+    # x_test = x_test.astype('float32')
+    # x_train /= 255
+    # x_test /= 255
+
+    print('x_train shape:', x_train.shape)
+    print(x_train.shape[0], 'train samples')
+    # print(x_test.shape[0], 'test samples')
+
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu',
+                     input_shape=x_train.shape[1:]))
+    # model.add(Conv2D(64, (3, 3), activation='relu'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1))
+
+    model.compile(loss=keras.losses.mean_squared_error,
+                  optimizer=keras.optimizers.RMSprop(),
+                  metrics=['accuracy'])
+
+    model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              verbose=1,
+              # validation_data=(x_test, y_test),
+              )
+    score = model.evaluate(x_train, y_train, verbose=0)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
