ac6f632d45a14d2a53c478d66d0ba835fa87e9bb
kyon <kyon.0308@gmail.com>
2018-01-28 21:45:13 +0900

    add conv and remove dropout

--- ml.py
+++ ml.py
@@ -137,12 +137,12 @@ if __name__ == '__main__':
     model = Sequential()
     model.add(Conv2D(32, kernel_size=(3, 3), activation='relu',
                      input_shape=x_train.shape[1:], name='conv2d_1'))
-    # model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu', name='conv2d_2'))
     # model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
+    # model.add(Dropout(0.25))
     model.add(Flatten())
     model.add(Dense(128, activation='relu', name='dense_1'))
-    model.add(Dropout(0.5))
+    # model.add(Dropout(0.5))
     model.add(Dense(1, name='dense_2'))
     model.summary(print_fn=log)
 
