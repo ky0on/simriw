8a2b2e41567cdae03f886422fbfad210f006188d
kyon <kyon.0308@gmail.com>
2018-03-20 23:55:35 +0900

    add padding=same

--- ml.py
+++ ml.py
@@ -162,8 +162,8 @@ if __name__ == '__main__':
         #3x3 conv2d
         model = Sequential()
         model.add(Conv2D(32, kernel_size=(3, 3), activation='relu',
-                         input_shape=x_train.shape[1:], name='conv2d_1'))
-        model.add(Conv2D(64, (3, 3), activation='relu', name='conv2d_2'))
+                         input_shape=x_train.shape[1:], name='conv2d_1', padding='same'))
+        model.add(Conv2D(64, (3, 3), activation='relu', name='conv2d_2', padding='same'))
         # model.add(MaxPooling2D(pool_size=(2, 2)))
         # model.add(Dropout(0.25))
         model.add(Flatten())
