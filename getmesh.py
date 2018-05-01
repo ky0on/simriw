3c323a26afb98eed8ef9a7ee6a1d4430673b0205
kyon <kyon.0308@gmail.com>
2018-05-01 18:15:03 +0900

    make getmesh.py fast by using pandas

--- getmesh.py
+++ getmesh.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""   """
+""" Download 1km-mesh data from NARO  """
 
 from __future__ import print_function
 import os
@@ -20,56 +20,33 @@ __autor__ = 'Kyosuke Yamamoto (kyon)'
 __date__ = '08 Oct 2017'
 
 
-def fetch(element, timedomain, lalodomain):
+def fetch(element, timedomain, lalodomain, interval):
+    ''' Download 1km-mesh data from NARO '''
     try:
         Msh, tim, lat, lon = AMD.GetMetData(element, timedomain, lalodomain)
+        print(f'Msh.shape (original, {element}): {Msh.shape}')
+        lat = lat[::interval]
+        lon = lon[::interval]
+        Msh = Msh[:, ::interval, ::interval]
+        print(f'Msh.shape (sliced, {element}): {Msh.shape}')
     except OSError:
-        print('Warning: {} in {} is not available. Filled with nan'.format(element, year))
-        ndays = AMD.timrange(*timedomain)
-        Msh = np.empty([len(ndays), 1000, 1000])   # approximate sizes of lat and lon
-        Msh[:, :] = None
-        lat = None
-        lon = None
-        tim = None
+        print(f'Warning: {element} in {year} is not available. Returned None.')
+        return None
 
-    dic = {
-        'lat': lat,
-        'lon': lon,
-        'tim': tim,
-        'mesh': Msh,
-        'element': element,
-    }
-    return dic
+    df = pd.DataFrame({
+        element: Msh.flatten(),
+        'lat': list(np.repeat(lat, len(lon))) * len(tim),
+        'lon': list(lon) * len(lat) * len(tim),
+        'date': np.repeat(tim, len(lat) * len(lon)),
+    })
+    print(f'{element} datafram done')
 
+    #set index
+    df.set_index(['date', 'lat', 'lon'], inplace=True)
+    print(f'{element} set_index done')
 
-def split_by_meshcode(lat, lon, tims, y, x, year, pref, df):
-    ''' '''
-    mesh = {d['element']: d['mesh'][:, y, x] for d in df}
-    mesh['DATE'] = tims
-    mesh = pd.DataFrame(mesh)
-    mesh.set_index('DATE', inplace=True)
-
-    #rename columns
-    mesh['DOY'] = mesh.index.dayofyear
-    mesh['YEAR'] = mesh.index.year
-    mesh.rename(columns={
-        'TMP_mea': 'T2M',
-        'TMP_max': 'T2MX',
-        'TMP_min': 'T2MN',
-        'GSR': 'swv_dwn',
-        'APCP': 'RAIN',
-        'RH': 'RH2M'}, inplace=True)
-
-    #save
-    meshcode = AMD.lalo2mesh(lat, lon)
-    outdir = os.path.join('meshdata', pref, str(year))
-    outcsv = os.path.join(outdir, f'{meshcode}.csv')
-    mkdir(outdir)
-    with open(outcsv, 'w') as f:
-        f.write('#config - lat:{}\n'.format(lat))
-        f.write('#config - lon:{}\n'.format(lon))
-        mesh.to_csv(f, float_format='%.3f')
-    # print('saved as', outcsv)
+    # df[(df.A>0) & (df.B>0)]
+    return df
 
 
 if __name__ == '__main__':
@@ -87,14 +64,10 @@ if __name__ == '__main__':
     #explore locations
     for pref in dataset.keys():
 
-        #filter by prefecture
-        if pref not in ('yamanashi', 'akita', 'niigata'):
-            continue
-
         #skip if data exists
-        if os.path.exists(os.path.join('meshdata', pref)):
-            print('skipped', pref)
-            continue
+        # if os.path.exists(os.path.join('meshdata', pref)):
+        #     print('skipped', pref)
+        #     continue
 
         #init
         lalodomain = [dataset[pref]['lat0'], dataset[pref]['lat1'],
@@ -105,15 +78,46 @@ if __name__ == '__main__':
         for year in range(1980, 2017):
 
             #access to server
-            timedomain = [f'{year}-05-01', f'{year}-12-31']
-            df = joblib.Parallel(n_jobs=6, verbose=1)(
-                joblib.delayed(fetch)(element, timedomain, lalodomain) for element in elements)
+            timedomain = [f'{year}-05-01', f'{year}-10-31']
+            dfs = joblib.Parallel(n_jobs=6, verbose=1)(
+                joblib.delayed(fetch)(element, timedomain, lalodomain, args.interval) for element in elements)
 
-            #split by meshcode
-            lats = df[0]['lat']
-            lons = df[0]['lon']
-            tims = df[0]['tim']
-            joblib.Parallel(n_jobs=-1, verbose=10)(
-                joblib.delayed(split_by_meshcode)(
-                    lats[y], lons[x], tims, y, x, year, pref, df)
-                for y in range(0, len(lats), args.interval) for x in range(0, len(lons), args.interval))
+            #assert
+            # for i in range(1, len(dfs)):
+            #     if dfs[i] is None:
+            #         continue
+            #     for col in ('date', 'lat', 'lon', 'meshcode'):
+            #         assert(np.all(dfs[0][col] == dfs[i][col]))
+
+            #concat
+            #TODO(kyon): concat by index is slow?
+            print('concat...')
+            df = pd.concat(dfs)
+            df.reset_index(inplace=True)
+            df['YEAR'] = df.date.dt.year
+            df['DOY'] = df.date.dt.dayofyear
+            print('done')
+
+            #rename columns
+            df.rename(
+                columns={
+                    'TMP_mea': 'T2M',
+                    'TMP_max': 'T2MX',
+                    'TMP_min': 'T2MN',
+                    'GSR': 'swv_dwn',
+                    'APCP': 'RAIN',
+                    'RH': 'RH2M'}, inplace=True)
+
+            #split by meshcode and save as csv
+            df_grouped = df.groupby(['lat', 'lon'])
+            for (lat, lon), row_idx in df_grouped.groups.items():
+                record = df.iloc[row_idx].copy()
+                meshcode = AMD.lalo2mesh(lat, lon)
+                record['meshcode'] = meshcode
+                outdir = os.path.join('meshdata', pref, str(year))
+                outcsv = os.path.join(outdir, f'{meshcode}.csv')
+                mkdir(outdir)
+                with open(outcsv, 'w') as f:
+                    f.write('#config - lat:{}\n'.format(lat))
+                    f.write('#config - lon:{}\n'.format(lon))
+                    record.to_csv(f, float_format='%.3f')
